245537666416ee5c5b7dff93d50a9167291be5a0
AshvathMurthy <92076903+AshvathMurthy@users.noreply.github.com>
2022-07-29 17:37:11 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,16 @@
+let ashvath_1 = { name: "ASH", email: "ashvathlevine23@gmail.com", state: "Tamilnadu" }; 
+for (let i in ashvath_1) 
+{ 
+    console.log(i, ":-", ashvath_1[i]); }
+
+
+
+
+    const names = { a: 'ash', z: 'vat', m: 'man' };
+    for (let value of Object.values(names)) {
+        console.log(value);
+    }
+
+
+   
+    
